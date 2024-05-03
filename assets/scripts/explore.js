c84245582c868bc3d0c14e1e07ec58494cfccd2f
Jacob Roner <jroner@ucsd.edu>
2024-05-02 21:37:01 -0700

    Finished Explore section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,46 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const img = document.querySelector('img');
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const button = document.querySelector('button');
+  let voices = [];
+
+  const loadVoices = () => {
+    voices = synth.getVoices();
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+        option.textContent = `${voice.name} (${voice.lang})`;
+        option.setAttribute('value', voice.name);
+        voiceSelect.appendChild(option);
+    });
+  };
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = loadVoices;
+  }
+
+  button.addEventListener('click', () => {
+    const textToSpeak = textArea.value;
+    const selectedVoiceName = voiceSelect.value;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    const selectedVoice = voices.find(voice => voice.name === selectedVoiceName);
+
+    utterance.voice = selectedVoice;
+    utterance.onstart = () => {
+        img.src = 'assets/images/smiling-open.png'; 
+    };
+    utterance.onend = () => {
+        img.src = 'assets/images/smiling.png';
+    };
+
+
+    synth.speak(utterance);
+  });
+
+
+  
 }
